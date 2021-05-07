150013ff7794142c4577eb4ed157e006b6289942
Elisa <bilingualedu@gmail.com>
2021-05-06 23:57:15 -0400

    Add: demofirst app view image of sea-horse

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Image } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Glow In The Blue</Text>
+      <Text style={{ fontSize: 25 }}>Glow In The Blue</Text>
+      <Image style={{ top: "5%" }} source={require("./assets/sea-horse.png")} />
       <StatusBar style="auto" />
     </View>
   )
