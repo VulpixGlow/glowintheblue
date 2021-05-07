0491d552781cb196c7231cac86b38364205b5686
ShannonCh <shannon26.c@gmail.com>
2021-05-07 17:49:55 -0400

    feat: firebase-setup

--- App.js
+++ App.js
@@ -1,33 +1,29 @@
-import { StatusBar } from "expo-status-bar"
-import React from "react"
-import { StyleSheet, Text, View, ImageBackground } from "react-native"
-import seahorse from './assets/seahorse.jpg'
-
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+const Stack = createStackNavigator();
 export default function App() {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
   return (
-
-    <ImageBackground source={seahorse} style={styles.container}>
-      <Text style={styles.text}>Glow In The Blue</Text>
-      <StatusBar style="auto" />
-    </ImageBackground>
-
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        { user ? (
+          <Stack.Screen name="Home">
+            {props => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    height: null,
-    width: null,
-    top:1,
-    opacity: 0.9
-  },
-  text: {
-    color: '#80daeb',
-    fontSize: 30,
-    fontWeight: '500',
-    backgroundColor: '#000060'
-  }
-})
