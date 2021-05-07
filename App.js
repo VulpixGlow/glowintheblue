5f76665686b253faad9a69260c82ee7c1bb8daa5
ShannonCh <shannon26.c@gmail.com>
2021-05-07 11:17:37 -0400

    feat: test background

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, ImageBackground } from "react-native"
+import seahorse from './assets/seahorse.jpg'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Glow In The Blue</Text>
+
+    <ImageBackground source={seahorse} style={styles.container}>
+      <Text style={styles.text}>Glow In The Blue</Text>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
+
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1e90ff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    height: null,
+    width: null,
+    top:1,
+    opacity: 0.9
+  },
+  text: {
+    color: '#80daeb',
+    fontSize: 30,
+    fontWeight: '500',
+    backgroundColor: '#000060'
   }
 })
