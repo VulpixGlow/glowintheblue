72e353ed51b62480e280aca1fef66c70a95a9798
Hannah Kemp <hfk@mail.usf.edu>
2021-05-17 11:31:05 -0400

    clean merge

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,15 @@
-export { default as LoginScreen } from "./LoginScreen/LoginScreen"
-
 export { default as HomeScreen } from "./HomeScreen/HomeScreen"
+
+export { default as Onboarding } from "./Onboarding/Onboarding"
+
+export { default as LoginScreen } from "./LoginScreen/LoginScreen"
 
 export { default as RegistrationScreen } from "./RegistrationScreen/RegistrationScreen"
 
-export { default as TimerScreen } from "./TimerScreen/TimerScreen"
+export { default as PointScreen } from "./PointScreen/PointScreen"
+
+export { default as GraphScreen } from "./GraphScreen/GraphScreen"
+
+export { default as StoreScreen } from "./StoreScreen/StoreScreen"
+
+export { default as FooterScreen } from "./FooterScreen/FooterScreen"
