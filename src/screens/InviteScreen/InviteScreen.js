1735ef6398614f42f19d53f36ea7d08d7ac0ee2e
chumilanesa <chumilanesa@gmail.com>
2021-05-19 12:32:52 -0400

    fix: View and icon bug

--- src/screens/InviteScreen/InviteScreen.js
+++ src/screens/InviteScreen/InviteScreen.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput} from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 import { Card, Input, Text, Button } from 'react-native-elements';
 import FooterScreen from '../FooterScreen/FooterScreen';
 import { useNavigation } from '@react-navigation/native';
-import { GrAdd } from 'react-icons/gr';
 
 export default function InviteScreen() {
   // const navigation = useNavigation();
@@ -13,21 +12,15 @@ export default function InviteScreen() {
       <Card>
         <View style={{ marginBottom: 160 }}>
           <Text h1>Invite your friends!</Text>
-          <Input 
-          placeholder='Email' 
-          // button={    <Button title='➕' type="outline" style={{width: 50, marginLeft: 300}}></Button>}>
-          rightIcon={{ name: "add", size: 24, color: "green" }}
- >
-          </Input>
+          <Input
+            placeholder='Email'
+            // button={    <Button title='➕' type="outline" style={{width: 50, marginLeft: 300}}></Button>}>
+            rightIcon={{ name: 'add', size: 24, color: 'green' }}></Input>
 
-          <Input placeholder='Email' 
-           rightIcon={{ name: "add", size: 24, color: "green" }}/>
-          <Input placeholder='Email' 
-           rightIcon={{ name: "add", size: 24, color: "green" }}/>
-          <Input placeholder='Email' 
-           rightIcon={{ name: "add", size: 24, color: "green" }}/>
-          <Input placeholder='Email' 
-           rightIcon={{ name: "add", size: 24, color: "green" }}/>
+          <Input placeholder='Email' rightIcon={{ name: 'add', size: 24, color: 'green' }} />
+          <Input placeholder='Email' rightIcon={{ name: 'add', size: 24, color: 'green' }} />
+          <Input placeholder='Email' rightIcon={{ name: 'add', size: 24, color: 'green' }} />
+          <Input placeholder='Email' rightIcon={{ name: 'add', size: 24, color: 'green' }} />
         </View>
       </Card>
       <FooterScreen />
@@ -43,7 +36,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     // marginTop: 7,
     // marginBottom: 7,
-    
+
     marginRight: 7,
     // padding: 20,
     // alignItems: 'center'
