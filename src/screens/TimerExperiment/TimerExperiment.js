446f76cf57f64732d03ae0fd0b7c7220a50eecae
Elisa <bilingualedu@gmail.com>
2021-05-21 12:11:16 -0400

    feat: add vibration to phone when user completes task

--- src/screens/TimerExperiment/TimerExperiment.js
+++ src/screens/TimerExperiment/TimerExperiment.js
@@ -1,64 +1,74 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { Text, View, Alert, SafeAreaView, Animated, Vibration, Item, TouchableHighlight, } from 'react-native'
-import { Button } from 'react-native-elements'
-import { useNavigation } from '@react-navigation/native'
-import { Picker } from '@react-native-picker/picker'
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
-import SelectDropdown from 'react-native-select-dropdown'
-import FooterScreen from '../FooterScreen/FooterScreen'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-import filterDataFunction from './filterDataFunction'
-import styles from './styles'
-import axios from 'axios'
+import React, { useEffect, useState, useRef } from 'react';
+// CAN  YOU FEEL THE VIBRATION?
+import {
+  Text,
+  View,
+  Alert,
+  SafeAreaView,
+  Animated,
+  Vibration,
+  Item,
+  TouchableHighlight
+} from 'react-native';
+import { Button } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import SelectDropdown from 'react-native-select-dropdown';
+import FooterScreen from '../FooterScreen/FooterScreen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import filterDataFunction from './filterDataFunction';
+import styles from './styles';
+import axios from 'axios';
 
 export default function TimerExperiment(props) {
-  console.log('TIMEREXPERIMENT COMPONENT PROPS', props)
+  console.log('TIMEREXPERIMENT COMPONENT PROPS', props);
 
-  const [userData, setUserData] = useState([])
-  const [isRunning, setRunning] = useState(false)
-  const [selectedValue, setSelectedValue] = useState(0)
-  const [points, setPoints] = useState(0)
+  const [userData, setUserData] = useState([]);
+  const [isRunning, setRunning] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(0);
+  const [points, setPoints] = useState(0);
 
-  const timerEmail = props.userData.extraData.email
-  console.log('timerEmail -->', timerEmail)
-  const pickerRef = useRef()
-  const navigation = useNavigation()
+  const timerEmail = props.userData.extraData.email;
+  console.log('timerEmail -->', timerEmail);
+  const pickerRef = useRef();
+  const navigation = useNavigation();
 
   // Category constants - Refactored
-  const [selectCat, setSelectedCat] = useState('')
-  const categories = ['Focus', 'Meditate', 'Move', 'Connect', 'Other']
+  const [selectCat, setSelectedCat] = useState('');
+  const categories = ['Focus', 'Meditate', 'Move', 'Connect', 'Other'];
 
   // Axios request for data
   const sessionData = async () => {
-    console.log('INSIDE SESSION DATA FUNCTION')
+    console.log('INSIDE SESSION DATA FUNCTION');
     try {
       // const { data } = await axios.get('http://localhost:8080/api/sessions')
-      const { data } = await axios.get('https://glowintheblue.herokuapp.com/api/sessions')
-      console.log('Data from Timer Component -->', data)
-      setUserData(data)
+      const { data } = await axios.get('https://glowintheblue.herokuapp.com/api/sessions');
+      console.log('Data from Timer Component -->', data);
+      setUserData(data);
     } catch (error) {
       if (error.response) {
         // There is an error response from the server
         // https://stackoverflow.com/questions/61116450/what-is-causing-an-unhandled-promise-rejection-undefined-is-not-an-object-eval
-        console.log('Error response from server', err.response.data)
+        console.log('Error response from server', err.response.data);
       } else if (error.request) {
         // The request was made but no response was received
-        console.log('No response was recieved', error.request)
+        console.log('No response was recieved', error.request);
       } else {
         // Some other errors
-        console.log('Error', error.message)
+        console.log('Error', error.message);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    sessionData()
-  }, [])
+    sessionData();
+  }, []);
 
   // Axios call to update data in db after each "session"
   const onConfirmCompleted = async total => {
     try {
-      setPoints(total)
+      setPoints(total);
       // http://localhost:8080/api/sessions/update
       await axios.put('https://glowintheblue.herokuapp.com/api/sessions/update', {
         email: timerEmail,
@@ -66,46 +76,46 @@ export default function TimerExperiment(props) {
         categoryName: selectCat,
         time: selectedValue,
         points: points
-      })
+      });
 
       // return the updated data for the timeline to reflect newly completed session
       // http://localhost:8080/api/sessions
-      const { data } = await axios.get('https://glowintheblue.herokuapp.com/api/sessions/')
-      console.log('Newly Completed Session -->', data)
+      const { data } = await axios.get('https://glowintheblue.herokuapp.com/api/sessions/');
+      console.log('Newly Completed Session -->', data);
       // update state with new data
-      setUserData(data)
+      setUserData(data);
     } catch (error) {
-      console.log('We got an error', error)
+      console.log('We got an error', error);
     }
-  }
+  };
 
-  let addPoints = 0
+  let addPoints = 0;
 
   switch (selectedValue) {
     case 10:
-      addPoints = 5
-      break
+      addPoints = 5;
+      break;
     case 20:
-      addPoints = 10
-      break
+      addPoints = 10;
+      break;
     case 30:
-      addPoints = 15
-      break
+      addPoints = 15;
+      break;
     case 40:
-      addPoints = 20
-      break
+      addPoints = 20;
+      break;
     case 50:
-      addPoints = 25
-      break
+      addPoints = 25;
+      break;
     case 60:
-      addPoints = 30
-      break
+      addPoints = 30;
+      break;
     default:
-      addPoints = 2
+      addPoints = 2;
   }
 
   // TotalPoint collected after each "session"
-  let totalPoints = points + addPoints
+  let totalPoints = points + addPoints;
 
   // This function invokes "onConfirmCompleted" function which creates another axios request to update data
   const createTwoButtonAlert = () =>
@@ -116,24 +126,24 @@ export default function TimerExperiment(props) {
         style: 'cancel'
       },
       { text: 'Glowing', onPress: () => onConfirmCompleted(totalPoints) }
-    ])
+    ]);
 
   // Jumps to this console.log after timerEmail console.log
-  console.log('MOST RECENT USER DATA', userData)
+  console.log('MOST RECENT USER DATA', userData);
 
   // how to access the user email => props.userData.extraData.email
   // let dataForTimeLine = filterDataFunction(userData, 'aavrahamy2x@webnode.com');
 
   // Data cleaning  prior to passing it down to graph - invoked when a user clicks on the footer button
-  let dataForTimeLine = filterDataFunction(userData, `${timerEmail}`)
+  let dataForTimeLine = filterDataFunction(userData, `${timerEmail}`);
 
   const children = ({ remainingTime }) => {
-    const hours = Math.floor(remainingTime / 3600)
-    const minutes = Math.floor((remainingTime % 3600) / 60)
-    const seconds = remainingTime % 60
+    const hours = Math.floor(remainingTime / 3600);
+    const minutes = Math.floor((remainingTime % 3600) / 60);
+    const seconds = remainingTime % 60;
 
-    return `${hours}:${minutes}:${seconds}`
-  }
+    return `${hours}:${minutes}:${seconds}`;
+  };
 
   return (
     <SafeAreaView>
@@ -175,9 +185,9 @@ export default function TimerExperiment(props) {
             key={selectedValue}
             duration={selectedValue}
             onComplete={() => {
-              setRunning(false)
-              Vibration.vibrate()
-              createTwoButtonAlert()
+              setRunning(false);
+              Vibration.vibrate();
+              createTwoButtonAlert();
             }}
             children
             size={180}
@@ -206,18 +216,18 @@ export default function TimerExperiment(props) {
               outerHeight: 40
             }}
             renderDropdownIcon={() => {
-              return <FontAwesome name='chevron-down' color={'#fff'} size={14} />
+              return <FontAwesome name='chevron-down' color={'#fff'} size={14} />;
             }}
             dropdownIconPosition={'right'}
             buttonTextStyle={{ color: '#fff' }}
             onSelect={(selectedItem, index) => {
-              setSelectedCat(selectedItem)
+              setSelectedCat(selectedItem);
             }}
             buttonTextAfterSelection={(selectedItem, index) => {
-              return selectedItem
+              return selectedItem;
             }}
             rowTextForSelection={(item, index) => {
-              return item
+              return item;
             }}
             dropdownStyle={{ backgroundColor: '#EFEFEF' }}
             rowStyle={{
@@ -253,5 +263,5 @@ export default function TimerExperiment(props) {
         />
       </View>
     </SafeAreaView>
-  )
+  );
 }
